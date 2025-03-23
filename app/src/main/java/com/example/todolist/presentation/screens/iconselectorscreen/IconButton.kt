926e578373f3e0e99f8c3f8568c2922e5203898d
Henrique Selau de Oliveira <hso.softwares@gmail.com>
2025-03-23 18:53:00 -0300

    add icon selector screen

---
+++ app/src/main/java/com/example/todolist/presentation/screens/iconselectorscreen/IconButton.kt
@@ -0,0 +1,55 @@
+package com.example.todolist.presentation.screens.iconselectorscreen
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IconButton(onIconClick:()-> Unit){
+
+
+    OutlinedButton(
+        onClick = onIconClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .padding(5.dp),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.outlinedButtonColors(containerColor = Color(20,20,20)),
+        border = BorderStroke(2.dp, Color(40,40,40, 255))
+
+    ) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Icon(
+                imageVector = Icons.Filled.Email,
+                modifier = Modifier
+                    .size(50.dp)
+                    .align(Alignment.Center),
+                contentDescription = "editButtons",
+                tint = Color.White,
+            )
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/example/todolist/presentation/screens/iconselectorscreen/IconSelectorScreen.kt
@@ -0,0 +1,37 @@
+package com.example.todolist.presentation.screens.iconselectorscreen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import com.example.todolist.presentation.viewmodel.TodoListViewModel
+import com.example.todolist.ui.theme.AppColor_Background
+
+@Composable
+fun IconSelectorScreen(viewModel: TodoListViewModel, navController: NavController){
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(100.dp),
+        modifier = Modifier.fillMaxSize().background(AppColor_Background).padding(start = 10.dp, end=10.dp, top = 50.dp),
+        //contentPadding = PaddingValues(vertical = 10.dp, horizontal = 10.dp)
+    ) {
+        items(100){
+            IconButton(onIconClick = {navController.popBackStack()})
+        }
+    }
+}
+
+    //Box(modifier = Modifier.fillMaxSize().background(Color.Red)){}
+
+
+
